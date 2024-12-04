0f67f3825b4a411bf125eea77e5cb8c867a110e4
MezinK <mezinkocahal@hotmail.com>
2024-12-04 17:21:13 +0100

    day 4

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use eyre::Result;
 mod solutions;
 
 fn main() -> Result<()> {
-    let input = fs::read_to_string("./src/input/day03.txt")?;
+    let input = fs::read_to_string("./src/input/day04.txt")?;
 
     let start = Instant::now();
 
-    let solution = solutions::day03::part2(&input);
+    let solution = solutions::day04::part2(&input);
 
     let elapsed = start.elapsed();
 
---
+++ src/solutions/day04.rs
@@ -0,0 +1,86 @@
+fn find_xmas(grid: &[Vec<char>]) -> usize {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let directions = [
+        (0, 1),   // Right
+        (0, -1),  // Left
+        (1, 0),   // Down
+        (-1, 0),  // Up
+        (1, 1),   // Down-Right
+        (1, -1),  // Down-Left
+        (-1, 1),  // Up-Right
+        (-1, -1), // Up-Left
+    ];
+
+    let word = "XMAS";
+    let word_len = word.len();
+    let word_chars: Vec<char> = word.chars().collect();
+
+    let mut count = 0;
+
+    for row_idx in 0..rows {
+        for col_idx in 0..cols {
+            for &(dir_r, dir_c) in &directions {
+                let mut matched = true;
+                for i in 0..word_len {
+                    let nr = row_idx as isize + i as isize * dir_r;
+                    let nc = col_idx as isize + i as isize * dir_c;
+
+                    if nr < 0 || nr >= rows as isize || nc < 0 || nc >= cols as isize {
+                        matched = false;
+                        break;
+                    }
+
+                    if grid[nr as usize][nc as usize] != word_chars[i] {
+                        matched = false;
+                        break;
+                    }
+                }
+                if matched {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
+}
+
+fn find_x_mas(grid: &[Vec<char>]) -> usize {
+    let height = grid.len();
+    let width = grid.first().unwrap().len();
+
+    let mut ans = 0;
+
+    for i in 1..height - 1 {
+        for j in 1..width - 1 {
+            if grid[i][j] != 'A' {
+                continue;
+            }
+
+            if (grid[i - 1][j - 1] == 'M' && grid[i + 1][j + 1] == 'S'
+                || grid[i - 1][j - 1] == 'S' && grid[i + 1][j + 1] == 'M')
+                && (grid[i - 1][j + 1] == 'M' && grid[i + 1][j - 1] == 'S'
+                    || grid[i - 1][j + 1] == 'S' && grid[i + 1][j - 1] == 'M')
+            {
+                ans += 1;
+            }
+        }
+    }
+
+    ans
+}
+
+fn create_grid_from_input(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|l| l.chars().collect()).collect()
+}
+
+pub fn part1(input: &str) -> usize {
+    let grid = create_grid_from_input(input);
+    find_xmas(&grid)
+}
+
+pub fn part2(input: &str) -> usize {
+    let grid = create_grid_from_input(input);
+    find_x_mas(&grid)
+}
